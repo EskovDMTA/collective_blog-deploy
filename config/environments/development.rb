3a5951954d6da8827ba0cf80e3652c17a69843b5
eskovdmt <DmtEskov@gmail.com>
2024-03-19 14:12:04 +0300

    feat: change host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.enable_reloading = true
-  config.hosts << 'collectiveblog-deploy-production.up.railway.app/'
+  config.hosts << "collectiveblog-deploy-production.up.railway.app"
 
   # Do not eager load code on boot.
   config.eager_load = false
